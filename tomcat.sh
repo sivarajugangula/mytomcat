33a2a92ab11cfd8d130f06477ed715dcd01aa28b
sivarajugangula <128023036+sivarajugangula@users.noreply.github.com>
2023-06-29 12:40:29 +0530

    Update tomcat.sh

--- tomcat.sh
+++ tomcat.sh
@@ -1,12 +1,11 @@
 sudo apt update 
-sudo apt install unzip -y
 sudo wget https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.76/bin/apache-tomcat-9.0.76.tar.gz
 sudo tar -xvzf apache-tomcat-9.0.76.tar.gz
 sudo mv apache-tomcat-9.0.76 /opt/tomcat9/
 sudo useradd -M -d /opt/tomcat9/ tomcat
 sudo  chown -R tomcat.tomcat /opt/tomcat9/ 
 sudo apt install default-jdk -y
-sudo cd /opt/tomcat9/bin/./startup.sh
+sudo ./opt/tomcat9/bin/startup.sh
 sudo sed -i '/<\/tomcat-users>/i \                                                     
 <role rolename="manager-gui"/>\
 <role rolename="admin-gui"/>\
